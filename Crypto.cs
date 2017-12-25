1de2b6f7a0e50defaba1bb1284e52a254a65aed4
chaytepsisi <34841956+chaytepsisi@users.noreply.github.com>
2017-12-25 22:38:45 +0300

    Add files via upload

---
+++ Crypto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace BasicDoubleHashing
+{
+    class Crypto
+    {
+        public static string sha256(string message)
+        {
+            SHA256Managed crypt = new SHA256Managed();
+            string hash = String.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(message), 0, Encoding.ASCII.GetByteCount(message));
+            foreach (byte theByte in crypto)
+            {
+                hash += Convert.ToString(theByte, 2).PadLeft(8, '0');
+            }
+            return hash;
+        }
+
+        public static string doubleSha256(string message)
+        {
+            string hashVal = sha256(message);
+            return sha256(hashVal);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace BasicDoubleHashing
+{
+    class Program
+    {
+        static void computehash()
+        {
+            Stopwatch stp = new Stopwatch();
+            stp.Start();
+            int counter = 0;
+            int hashCounter = 0;
+            int max = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                stp.Restart();
+                while (stp.ElapsedMilliseconds < 60000)
+                {
+                    string hashValue = Crypto.doubleSha256((hashCounter++).ToString());
+                    counter = 0;
+                    while (hashValue[0] == '0')
+                    {
+                        hashValue = hashValue.Substring(1);
+                        counter++;
+                    }
+                    if (counter > max)
+                        max = counter;
+                }
+                Console.WriteLine(max);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            computehash();
+        }
+    }
+}
